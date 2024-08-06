350a9dda61038393d6237d59ddea9fc942b56735
Hab <hab.collector@imrengineering.com>
2024-08-06 14:02:47 -0400

    Changes to gitignore

--- Debug/Core/Src/Application/TerminalEmulatorSupport.d
+++
@@ -1,4 +0,0 @@
-Core/Src/Application/TerminalEmulatorSupport.o: \
- ../Core/Src/Application/TerminalEmulatorSupport.c \
- ../Core/Inc/Application/TerminalEmulatorSupport.h
-../Core/Inc/Application/TerminalEmulatorSupport.h:
--- Debug/Core/Startup/startup_stm32l4r5vgtx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32l4r5vgtx.o: \
- ../Core/Startup/startup_stm32l4r5vgtx.s
